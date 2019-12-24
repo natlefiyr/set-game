1a7a2a858cb5dc64e2558f4e2b7bf389a88ad247
Natasha Eilbert <neilbert@athenahealth.com>
2019-12-24 13:32:24 -0500

    Add some card functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import cloneDeep from "lodash/cloneDeep";
+
+const SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"];
+const NUMBERS = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const SUIT_ABBREVIATIONS = ["S", "H", "C", "D"];
+
+function displayCards(cards) {
+  return (
+    <ul>
+      {cards.map(card => (
+        <li>
+          {card.number}-{SUIT_ABBREVIATIONS[card.suitIndex]}
+        </li>
+      ))}
+    </ul>
+  );
+}
+function getDeck() {
+  let cards = [];
+  for (let suitIndex = 0; suitIndex < SUITS.length; suitIndex++) {
+    for (let numIndex = 0; numIndex < NUMBERS.length; numIndex++) {
+      cards.push({
+        suit: SUITS[suitIndex],
+        number: NUMBERS[numIndex],
+        suitIndex: suitIndex,
+        numberIndex: numIndex
+      });
+    }
+  }
+  return cards;
+}
+function shuffle(cards) {
+  const cardsCopy = cloneDeep(cards);
+  const shuffledCards = [];
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    let index = Math.floor(Math.random() * cardsCopy.length);
+    shuffledCards.push(cardsCopy[index]);
+    cardsCopy.splice(index, 1);
+  }
+  console.log(shuffledCards);
+  return shuffledCards;
+}
+function deal(cards) {
+  // For now just initial deal
+}
+function sort(cards) {}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const deck = getDeck();
+  const shuffledCards = shuffle(deck);
+  const displayableCards = displayCards(shuffledCards);
+  return <div className="App">{displayableCards}</div>;
 }
 
 export default App;
